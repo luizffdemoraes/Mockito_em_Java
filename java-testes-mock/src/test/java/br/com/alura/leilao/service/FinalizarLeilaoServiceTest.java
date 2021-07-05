b96fca564f0138e28ecd18f208aa256d87da4030
Luiz Moraes <lffm1994@gmail.com>
2021-07-05 13:36:37 -0300

    Manipulando o comportamento do mock

--- java-testes-mock/src/test/java/br/com/alura/leilao/service/FinalizarLeilaoServiceTest.java
+++ java-testes-mock/src/test/java/br/com/alura/leilao/service/FinalizarLeilaoServiceTest.java
@@ -1,11 +1,10 @@
 package br.com.alura.leilao.service;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -29,14 +28,23 @@ class FinalizarLeilaoServiceTest {
 		MockitoAnnotations.initMocks(this);
 		this.service = new FinalizarLeilaoService(leilaoDao);
 	}
-	
 
 	@Test
 	void deveriaFinalizarUmLeilao() {
 		List<Leilao> leiloes = leiloes();
+		
+		Mockito.when(leilaoDao.buscarLeiloesExpirados())
+		.thenReturn(leiloes);
+		
 		service.finalizarLeiloesExpirados();
+		
+		Leilao leilao = leiloes.get(0);
+		Assert.assertTrue(leilao.isFechado());
+		Assert.assertEquals(new BigDecimal("900"), 
+				leilao.getLanceVencedor().getValor());
+		
+		Mockito.verify(leilaoDao).salvar(leilao);
 	}
-	
 	
 	private List<Leilao> leiloes() {
 		List<Leilao> lista = new ArrayList<>();
